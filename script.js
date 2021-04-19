10828db2fe3db87eb674aa5f85abccea33a3f3b9
dylneed <63551389+dylneed@users.noreply.github.com>
2021-04-18 22:19:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var aWidth;
+var aHeight;
 
+var links;
+var help;
+
+window.addEventListener('load',init);
+window.addEventListener('resize', layoutPage);
+
+function init() {
+	links = document.getElementById("links");
+	help = document.getElementById("help");
+	layoutPage();
+}
+
+function layoutPage() {
+	aWidth = innerWidth;
+	aHeight = innerHeight;
+	//help.innerHTML += " " + aWidth;
+}
