8025865bc636e4b6390c2e61bea0d7e9f2dfa410
Dylan Needleman <dylneed@gmail.com>
2025-08-17 17:57:55 -0400

    add a bunch of non-selected electronic works

--- src/pages/Works/works/electronic.jsx
+++ src/pages/Works/works/electronic.jsx
@@ -17,11 +17,32 @@ const electronic = {
       url: "https://dylanneedleman.bandcamp.com/track/hen-to-p-n"
     },
     {
+      title: "Sanguine Dreams",
+      year: 2024,
+      url: "https://dylanneedleman.bandcamp.com/track/sanguine-dreams"
+    },
+    {
       title: "Sonatina for Paul (Passing Through)",
       year: 2024,
       selected:true,
       id: "sonatina-for-paul",
       url: "https://dylanneedleman.bandcamp.com/track/sonatina-for-paul-passing-through"
+    },
+    {
+      title: "Euthanized in Gnome Regalia",
+      year: 2024,
+      url: "https://dylanneedleman.bandcamp.com/track/euthanized-in-gnome-regalia"
+    },
+    {
+      title: "Diana, It's Michelle",
+      year: 2024,
+      id:"diana-its-michelle",
+      url: "https://dylanneedleman.bandcamp.com/track/diana-its-michelle"
+    },
+    {
+      title: "West",
+      year: 2023,
+      url: "https://dylanneedleman.bandcamp.com/track/west"
     },
     {
       title: "The Whalefall",
